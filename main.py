301a51bc0bc8afb5e64263fe4986a1f1473592d7
Ayush0901 <73170707+Ayush0901@users.noreply.github.com>
2021-11-10 21:37:04 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from random import randint
+import os
 
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 API_KEY = "Tw@999#"
